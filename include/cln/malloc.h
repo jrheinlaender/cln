d02fb1fd87e7b45b322196b478dfc7becbe1c4bd
Jan Rheinländer <jrheinlaender@users.sourceforge.net>
2018-04-25 20:47:48 +0200

    Remove user-definable malloc_hook and free_hook to avoid a static initialization fiasco when static variables of GiNaC are initialized (e.g. integral::relative_integration_error)

--- include/cln/malloc.h
+++ include/cln/malloc.h
@@ -7,10 +7,20 @@
 
 namespace cln {
 
+#ifdef _MSC_VER
+// Avoid a static initialization fiasco when static variables of GiNaC are initialized (e.g. integral::relative_integration_error)
+extern void* malloc_hook (size_t size);
+
+extern "C" void free (void* ptr);
+inline void free_hook (void* ptr) {
+	free(ptr);
+}
+#else
 // Function like malloc() which returns aligned memory of size (> 0) bytes.
 extern void* (*malloc_hook) (size_t size);
 // Function like free() which makes available for reuse such memory.
 extern void (*free_hook) (void* ptr);
+#endif
 
 }  // namespace cln
 
